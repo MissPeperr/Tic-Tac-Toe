aef667b650994f20a0443a8fcf0c924d01837d44
Madi Peper <madisonpeper@gmail.com>
2019-01-21 12:12:48 -0600

    edited comments in main.js

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-// # Tic Tac Toe
-
-// 1. Create a grid of nine boxes
-// 2. When any box is clicked a red `X` appears
-// 3. When the next box is clicked a Blue `O` appears
-// 4. #1 and #2 are repeated until a row of 3 of the same character is created
-// 5. When a win happens:
-//     1. Alert the players who won
-//     2. Disallow any further clicks on the game board
-//     3. Display a `Play Again` button that resets the game
-// 6. For a tie:
-//     1. Alert players that game has ended in a tie
-//     2. Display a `Play Again` button that resets the game
-
 // making some global variables
 let numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let counter = 0;
@@ -42,7 +28,6 @@ gameContainer.addEventListener("click", (event) => {
             if(event.target.id === `box-${num}`){
 
                 let targetBox = document.querySelector(`#box-${num}`);
-                // FYI counter goes up even when the same box is clicked again
                 if(counter % 2 === 0 && targetBox.innerHTML === ""){
                     targetBox.setAttribute("style", "color: darkred;");
                     targetBox.innerHTML = "X";
